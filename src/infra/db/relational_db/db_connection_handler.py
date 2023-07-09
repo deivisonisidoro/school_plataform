480cce8073cb4d834b8e7b30f4943ffa4e9e130a
Deivison Isidoro <deivisonisidoro@gmail.com>
2023-07-09 12:00:37 -0300

    feat: creating function to create db

--- src/infra/db/relational_db/db_connection_handler.py
+++ src/infra/db/relational_db/db_connection_handler.py
@@ -1,17 +1,39 @@
 from sqlalchemy.orm import Session
-
-from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import create_engine
-
 from contextlib import contextmanager
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 from .declarative_base import Base
 
 
 class DBConnectionHandler:
-    def __init__(self, connection_string: str = "sqlite:///:memory:"):
+    """
+    Handles the database connection and session management.
+    """
+
+    def __init__(
+        self,
+        connection_string: str = "sqlite:///:memory:",
+        base: Base = Base,
+    ):
+        """
+        Initialize the DBConnectionHandler.
+
+        Args:
+            connection_string (str, optional): The database connection string. Defaults to "sqlite:///:memory:".
+            base (Base, optional): The SQLAlchemy declarative base class. Defaults to Base.
+        """
         self.engine = create_engine(connection_string)
+        self.base = base
+
+    def create_db(self):
+        """
+        Create all database tables.
+
+        This function creates all database tables defined in the models.py file, based on the metadata created by the declarative base.
+
+        """
+        self.base.metadata.create_all(bind=self.engine)
 
     @contextmanager
     def get_db(self) -> Session:
